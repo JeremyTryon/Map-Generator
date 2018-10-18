8ec98d91ee8d0e99451f3892362c7f1dca2cb6df
JeremyTryon <jeremytryon1@gmail.com>
2018-10-17 22:41:30 -0500

    rough Cell/City

---
+++ Tiling Engine/Tiling Engine/Cell.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tiling_Engine
+{
+    class Cell
+    {
+        private Label _label;
+        private bool _visible;
+        public enum Region{N,G,D,M,O,T};
+        private Tuple<int, int> _coor;
+
+        public Cell(int x, int y)
+        {
+            _coor = new Tuple<int, int>(x, y);
+            _label = new Label();
+        }
+
+        public void ChangeColor(Region R)
+        {
+            if(R == Region.N)
+            {
+                _label.BackColor = System.Drawing.Color.DarkGray;
+            }
+            else if (R == Region.G)
+            {
+                _label.BackColor = System.Drawing.Color.Green;
+            }
+            else if (R == Region.D)
+            {
+                _label.BackColor = System.Drawing.Color.Green;
+            }
+            else if (R == Region.M)
+            {
+                _label.BackColor = System.Drawing.Color.LightGray;
+            }
+            else if (R == Region.O)
+            {
+                _label.BackColor = System.Drawing.Color.Blue;
+            }
+            else if (R == Region.T)
+            {
+                _label.BackColor = System.Drawing.Color.LightBlue;
+            }
+        }
+
+        public bool IsVisible()
+        {
+            return _visible;
+        }
+
+        public void setVisible(bool b)
+        {
+            _visible = b;
+        }
+    }
+}
---
+++ Tiling Engine/Tiling Engine/City.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tiling_Engine
+{
+    class City
+    {
+        private Cell[,] _grid;
+        private List<City> _towns;
+        private Tuple<int, int> _coor;
+
+        public City(int x, int y, int size)
+        {
+            _grid = new Cell[size,size];
+            _coor = new Tuple<int, int>(x, y);
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j<size; j++)
+                {
+                    _grid[i, j] = new Cell(i, j);
+                }
+            }
+            Edit();
+        }
+
+        public void Edit()
+        {
+            //opens map in the editor and sets map to old map
+        }
+
+        public Tuple<int,int> GetLocation()
+        {
+            return _coor;
+        }
+
+    }
+}
